c86abb4c06972c36a23b9e0b58ba17af7eaaeb6d
SomeDonk <jswense2@byu.edu>
2019-11-08 04:40:37 +0000

    It working now

--- app.js
+++ app.js
@@ -3,18 +3,25 @@ const express = require('express')
 const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static('public'));
 
 let kittykount = 0;
 const port = 4200
 
-app.get('/api/kittykount', (req, res) => {
-  res.send(kittykount);
+app.get('/api/kittykount', (req, res) =>
+{
+    console.log("in kittykount GET");
+    res.send(kittykount.toString());
 });
 
-app.post('/api/kittykount', (req, res) => {
-  kittykount++;
+app.post('/api/kittykount', (req, res) =>
+{
+    console.log("in kittykount POST");
+    kittykount++;
+    res.send(kittykount.toString());
 });
 
 app.listen(port, () => console.log('Server listening on port 4200!'));
--- public/script.js
+++ public/script.js
@@ -1,29 +1,44 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    kittykount: 0,
-  },
-  created() {
-    this.getKitties();
-  },
-  methods: {
-    async getKitties() {
-      try {
-        let response = await axios.get("http://localhost:4200/api/kittykount");
-        this.kittykount = response.data;
-        return true;
-      } catch (error) {
-        console.log(error);
-      }
+var app = new Vue(
+{
+    el: '#app',
+    data:
+    {
+        kittykount: 0,
     },
-    async kittyKlicked() {
-      try {
-        let response = await axios.post("http://localhost:4200/api/kittykount", {});
+    created()
+    {
         this.getKitties();
-        return true;
-      } catch (error) {
-        console.log(error);
-      }
     },
-  }
+    methods:
+    {
+        async getKitties()
+        {
+            try
+            {
+                let response = await axios.get("http://cs260.jaredsw.com:4200/api/kittykount");
+                this.kittykount = response.data;
+                console.log("In getKitties, kittykount = " + this.kittykount);
+                return true;
+            }
+            catch (error)
+            {
+                console.log(error);
+            }
+        },
+        async kittyKlicked()
+        {
+            console.log("In kittyKlicked, kittykount = " + this.kittykount);
+            try
+            {
+                let response = await axios.post("http://cs260.jaredsw.com:4200/api/kittykount", {});
+                console.log(response.data);
+                this.kittykount = response.data;
+                return true;
+            }
+            catch (error)
+            {
+                console.log(error);
+            }
+        },
+    }
 });
