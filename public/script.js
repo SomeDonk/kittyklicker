bb6272498b60239a70939a02d38ef6a0d75a50d4
SomeDonk <jswense2@byu.edu>
2019-11-08 05:42:35 +0000

    fixed upgrade and some css

--- public/script.js
+++ public/script.js
@@ -16,6 +16,7 @@ var app = new Vue({
         let response1 = await axios.get("http://cs260.jaredsw.com:4200/api/klickpower");
         this.klickPower = response1.data;
         let response2 = await axios.get("http://cs260.jaredsw.com:4200/api/klickupgradecost");
+        console.log(response2.data);
         this.klickUpgradeCost = response2.data;
         console.log("In getKitties, kittykount = " + this.kittykount);
         return true;
@@ -43,7 +44,7 @@ var app = new Vue({
           this.klickPower = this.klickPower * 2;
           this.kittykount = this.kittykount - this.klickUpgradeCost;
           this.klickUpgradeCost = this.klickUpgradeCost * 10;
-          let response = await axios.post("http://cs260.jaredsw.com:4200/api/doublepower", {klickpower: this.klickPower, kittykount: this.kittykount, klickupgradecost: this.klickUgradeCost});
+          let response = await axios.post("http://cs260.jaredsw.com:4200/api/doublepower", {klickpower: this.klickPower, kittykount: this.kittykount, klickupgradecost: this.klickUpgradeCost});
         }
       }
       catch (error) {
