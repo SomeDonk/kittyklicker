79336082e1e88a16520929d6e5c992a47bfe7e55
Stephen Howard <stephenhoward330@gmail.com>
2019-11-08 05:16:31 +0000

    added button

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
 let kittykount = 0;
+let klickpower = 1;
+let klickupgradecost = 10;
 const port = 4200
 
 app.get('/api/kittykount', (req, res) =>
@@ -17,11 +19,33 @@ app.get('/api/kittykount', (req, res) =>
     res.send(kittykount.toString());
 });
 
+app.get('/api/klickpower', (req, res) =>
+{
+    console.log("in klickpower GET");
+    res.send(klickpower.toString());
+});
+
+app.get('/api/klickupgradecost', (req, res) =>
+{
+    console.log("in klickupgradecost GET");
+    res.send(klickupgradecost.toString());
+});
+
 app.post('/api/kittykount', (req, res) =>
 {
     console.log("in kittykount POST");
-    kittykount++;
+    kittykount += req.body.klickpower;
     res.send(kittykount.toString());
 });
+
+app.post('/api/doublepower', (req, res) =>
+{
+    console.log("in doublepower POST");
+    kittykount = req.body.kittykount;
+    klickpower = req.body.klickpower;
+    klickupgradecost = req.body.klickupgradecost;
+    res.send(kittykount.toString());
+});
+
 
 app.listen(port, () => console.log('Server listening on port 4200!'));
--- public/script.js
+++ public/script.js
@@ -1,44 +1,54 @@
-var app = new Vue(
-{
-    el: '#app',
-    data:
-    {
-        kittykount: 0,
+var app = new Vue({
+  el: '#app',
+  data: {
+    kittykount: 0,
+    klickPower: 1,
+    klickUgradeCost: 10,
+  },
+  created() {
+    this.getKitties();
+  },
+  methods: {
+    async getKitties() {
+      try {
+        let response = await axios.get("http://cs260.jaredsw.com:4200/api/kittykount");
+        this.kittykount = response.data;
+        response = await axios.get("http://cs260.jaredsw.com:4200/api/klickpower");
+        this.klickPower = response.data;
+        response = await axios.get("http://cs260.jaredsw.com:4200/api/klickupgradecost");
+        this.klickUgradeCost = response.data;
+        console.log("In getKitties, kittykount = " + this.kittykount);
+        return true;
+      }
+      catch (error) {
+        console.log(error);
+      }
     },
-    created()
-    {
-        this.getKitties();
+    async kittyKlicked() {
+      console.log("In kittyKlicked, kittykount = " + this.kittykount);
+      try {
+        let response = await axios.post("http://cs260.jaredsw.com:4200/api/kittykount", { klickpower: this.klickPower });
+        console.log(response.data);
+        this.kittykount = response.data;
+        return true;
+      }
+      catch (error) {
+        console.log(error);
+      }
     },
-    methods:
-    {
-        async getKitties()
-        {
-            try
-            {
-                let response = await axios.get("http://cs260.jaredsw.com:4200/api/kittykount");
-                this.kittykount = response.data;
-                console.log("In getKitties, kittykount = " + this.kittykount);
-                return true;
-            }
-            catch (error)
-            {
-                console.log(error);
-            }
-        },
-        async kittyKlicked()
-        {
-            console.log("In kittyKlicked, kittykount = " + this.kittykount);
-            try
-            {
-                let response = await axios.post("http://cs260.jaredsw.com:4200/api/kittykount", {});
-                console.log(response.data);
-                this.kittykount = response.data;
-                return true;
-            }
-            catch (error)
-            {
-                console.log(error);
-            }
-        },
+    async doublePower() {
+      console.log("In double power");
+      try {
+        if (this.kittykount >= this.klickUgradeCost) {
+          this.klickPower = this.klickPower * 2;
+          this.kittykount = this.kittykount - this.klickUgradeCost;
+          this.klickUgradeCost = this.klickUgradeCost * 10;
+          let response = await axios.post("http://cs260.jaredsw.com:4200/api/doublepower", {klickpower: this.klickPower, kittykount: this.kittykount, klickupgradecost: this.klickUgradeCost});
+        }
+      }
+      catch (error) {
+        console.log(error);
+      }
     }
+  }
 });
